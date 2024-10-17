b0502f3790fa8169b3c1ccac3470b481acd1b320
Nicolas Daldegan <nicolasdaldega@gmail.com>
2024-10-17 10:40:14 -0300

    feat: final script go, ajustar as consultas e separar execuções rest/graphql

--- go-tcc/src/app/app.go
+++ go-tcc/src/app/app.go
@@ -3,20 +3,31 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
+	"os"
 	"time"
-	"unsafe"
 )
 
-var client = &http.Client{}
+var client = &http.Client{Timeout: 10 * time.Second}
 
 func main() {
-	var GITHUB_REST_URL string = "https://api.github.com/users/Netflix/repos?per_page=5"
+	var GITHUB_REST_URL string = "https://api.github.com/users/nicolasdaldegan/repos?per_page=5"
 
-	for i := 0; i < 0; i++ {
+	file, err := os.OpenFile("resultados.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		log.Fatalf("Erro ao abrir arquivo: %v", err)
+	}
+
+	defer file.Close()
+
+	for i := 0; i < 10; i++ {
 		size, time := doRest(GITHUB_REST_URL)
-		fmt.Println("Size: ", size)
-		fmt.Println("Time: ", time/100000)
+		result := fmt.Sprintf("REST -> Size: %d bytes, Time: %v\n", size, time)
+		fmt.Print(result)
+		writeToFile(file, result)
 	}
 
 	var GITHUB_GRAPHQL_URL string = "https://api.github.com/graphql"
@@ -25,48 +36,78 @@ func main() {
 
 	for i := 0; i < 10; i++ {
 		size, time := doGraphQL(GITHUB_GRAPHQL_URL, body)
-		fmt.Println("Size: ", size)
-		fmt.Println("Time: ", time/100000)
+		result := fmt.Sprintf("GraphQL -> Size: %d bytes, Time: %v\n", size, time)
+		fmt.Print(result)
+		writeToFile(file, result)
 	}
 
 }
 
-func doGraphQL(url_request string, body string) (uintptr, int) {
+func doGraphQL(url_request string, body string) (int, time.Duration) {
 
 	payload := bytes.NewBufferString(body)
 
-	req, _ := http.NewRequest(http.MethodPost, url_request, payload)
+	req, err := http.NewRequest(http.MethodPost, url_request, payload)
+
+	if err != nil {
+		log.Printf("Erro ao criar a request: %v", err)
+		return 0, 0
+	}
 
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Bearer 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")
 
 	time_init := time.Now()
+
 	resp, err := client.Do(req)
-	time_end := time.Now()
+
+	duration := time.Since(time_init)
 
 	if err != nil {
-		panic(err)
+		log.Printf("Erro ao executar requisição GraphQL: %v", err)
+		return 0, 0
 	}
-
-	fmt.Println("Response: ", resp.StatusCode)
 
 	defer resp.Body.Close()
 
-	return unsafe.Sizeof(resp.Request.RequestURI), int(time_end.Nanosecond() - time_init.Nanosecond())
+	body_response, err := io.ReadAll(resp.Body)
+
+	if err != nil {
+		log.Printf("Erro lendo resposta GraphQL: %v", err)
+		return 0, 0
+	}
+
+	return len(body_response), duration
 }
 
-func doRest(url_request string) (uintptr, int) {
+func doRest(url_request string) (int, time.Duration) {
 
 	time_init := time.Now()
-	resp, err := http.Get(url_request)
-	time_end := time.Now()
+
+	resp, err := client.Get(url_request)
+
+	duration := time.Since(time_init)
 
 	if err != nil {
-		panic(err)
+		log.Printf("Erro ao executar requisição REST: %v", err)
+		return 0, 0
 	}
 
 	defer resp.Body.Close()
 
-	return unsafe.Sizeof(resp.Request.RequestURI), int(time_end.Nanosecond() - time_init.Nanosecond())
+	body, err := io.ReadAll(resp.Body)
 
+	if err != nil {
+		log.Printf("Erro lendo resposta REST: %v", err)
+		return 0, 0
+	}
+
+	return len(body), duration
+
+}
+
+func writeToFile(file *os.File, data string) {
+	if _, err := file.WriteString(data); err != nil {
+		log.Printf("Erro ao escrever no arquivo: %v", err)
+	}
 }
